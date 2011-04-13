dc0ff568c1f00b86ec4ff9adedacca97391e6283
unknown <multi2@.pensiero>
2011-04-13 09:52:54 +0200

    initial commint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DavelabNet::Application.config.secret_token = '7b877167fd0e5c90b1717e9161018a912cbd7183e6fe3acabdee1b6a433c4b15a8e61a81fdae4d476abd3a995122efe05712ec8987e6af66ee79bad1bf1eed05'
